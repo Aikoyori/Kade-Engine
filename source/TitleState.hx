946e544801efec970e309e6e84f7e11c256ebdaf
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-05-02 00:05:48 +0700

    crash fix attempt #3

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -388,13 +388,22 @@ class TitleState extends MusicBeatState
 			logoBl.updateHitbox();
 			add(logoBl);
 		}
+		if(gfDance!=null){
 		danceLeft = !danceLeft;
-
+		
 		if (danceLeft)
 			gfDance.animation.play('danceRight');
 		else
 			gfDance.animation.play('danceLeft');
-
+		}
+		else{
+			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+			gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+			gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 34, false);
+			gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 34, false);
+			gfDance.antialiasing = true;
+			add(gfDance);
+			}
 		FlxG.log.add(curBeat);
 
 		switch (curBeat)
