3fdc69d2efc15b7ae8f6ffca228f27e43300ed31
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-05-01 22:11:25 +0700

    intro shit did happen but didn't crash so here's another code for comparison <3

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -145,13 +145,13 @@ class TitleState extends MusicBeatState
 			// IF THIS PR IS HERE IF ITS ACCEPTED UR GOOD TO GO
 			// https://github.com/HaxeFlixel/flixel-addons/pull/348
 
-			// var music:FlxSound = new FlxSound();
-			// music.loadStream(Paths.music('freakyMenu'));
-			// FlxG.sound.list.add(music);
-			// music.play();
-			FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
+			var music:FlxSound = new FlxSound();
+			music.loadStream(Paths.music('freakyMenu'));
+			FlxG.sound.list.add(music);
+			music.play();
+			//FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 
-			FlxG.sound.music.fadeIn(4, 0, 0.7);
+			//FlxG.sound.music.fadeIn(4, 0, 0.7);
 		}
 
 		Conductor.changeBPM(140);
@@ -380,7 +380,13 @@ class TitleState extends MusicBeatState
 			logoBl.animation.play('bump');
 		}
 		else{
-			startIntro();
+			logoBl = new FlxSprite(1, 1);
+			logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
+			logoBl.antialiasing = true;
+			logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
+			logoBl.animation.play('bump');
+			logoBl.updateHitbox();
+			add(logoBl);
 		}
 		danceLeft = !danceLeft;
 
